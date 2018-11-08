1d90d9f0c7b5fc86ce0ecb502641aecb29751ddd
jbettaque <bettaque@googlemail.com>
2018-11-08 01:16:41 +0100

    added explanations

--- password-generator.py
+++ password-generator.py
@@ -4,17 +4,32 @@ import re
 
 
 def request_len():
+
+    """
+    Prompts the user for the password length and returns the length as an int or -1 if input was invalid
+    :rtype: int
+    """
+
     length = input("Type in your desired password length:\n")
 
+    # if the input cannot be converted to an integer, it therefore is not a number and invalid
     try:
         length = int(length)
         return length
-    except Exception as e:
+    except:
         print("Invalid input for password length.")
         return -1
 
 
 def request_methods():
+
+    """
+    Prompts the user for password strength and returns list containing the pool of chars to generate from
+    (string list). Will repromt if input is invalid.
+    :rtype: list
+    """
+
+    # initializing spaces (methods)
     chars = "abcdefghijklmnopqrstuvwxyzäöü"
     chars_upper = chars.upper()
     numbers = "1234567890"
@@ -30,13 +45,18 @@ def request_methods():
     # if "," or "1" or "2" or "3" or "4" not in strength or strength[0] not in ["1", "2", "3", "4"]:
     #     print("Invalid input for strength.")
     #     quit()
+    # Just for demonstration purposes, way worse that regex and also not complete
 
-    if not re.match(r'([1234])|([1234],)+', strength):  # best site ever: https://regex101.com/
+    # check if input is correct with regex
+    # best site ever: https://regex101.com/
+    if not re.match(r'([1234])|([1234],)+', strength):
         print("Invalid input for password strength.")
         request_methods()
     else:
         strength = strength.split(",")
         methods = []
+
+        # replace input with actual methods
         for method_number in strength:
             if method_number == "1":
                 methods.append(chars)
@@ -50,17 +70,25 @@ def request_methods():
 
 
 def main():
+    """
+    Main Function
+    """
     print("-----------------------")
     print("PASSWORD GENERATOR v1.0")
     print("-----------------------")
     length = 0
+
+    # request_len returns -1 if input was invalid. Requests Length until valid value was returned
     while(length <= 0):
         length = request_len()
 
     methods = request_methods()
-    password = ""
-    for i in range(length):
 
+    password = ""
+    # for "length" chars
+    # choose (pseudo) random method
+    # generate char with chosen method and add it to password
+    for i in range(length):
         method = random.choice(methods)
         random_char = random.choice(method)
         password = password + random_char
